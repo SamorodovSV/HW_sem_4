898dd6e55becc601f62564148dab5f1b3277faa0
Samorodov Sergey <samorodovsv@yandex.ru>
2022-07-11 04:03:21 +0300

    Добавили третью задачу

--- Program.cs
+++ Program.cs
@@ -2,12 +2,37 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-// Console.WriteLine("Введите число, которое нужно возвести в степень:");
-// int A = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите степень:");
-// int B = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine(A +" ^ " + B + " = " + Math.Pow(A, B));
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         Console.Clear();
+//         Console.WriteLine("Введите число, которое нужно возвести в степень:");
+//         int A = Convert.ToInt32(Console.ReadLine());
+//         Console.WriteLine("Введите степень:");
+//         int B = Convert.ToInt32(Console.ReadLine());
+//         Console.WriteLine(A + " ^ " + B + " = " + Math.Pow(A, B));
+//     }
+// }
 
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         Console.WriteLine("Введите число, которое нужно возвести в степень:");
+//         int A = Convert.ToInt32(Console.ReadLine());
+//         Console.WriteLine("Введите степень:");
+//         int B = Convert.ToInt32(Console.ReadLine());
+//         int i = 1;
+//         int C = 0;
+//         while (i <= B)
+//         {
+//             C = A * A;
+//             i++;
+//         }
+//         Console.WriteLine(A + ", " + B + "-> " + C);
+//     }
+// }
 
 
 
@@ -16,45 +41,87 @@
 // 82 -> 10
 // 9012 -> 12
 
-// void SumDigitsInNumber ( int number){ 
-// while (number < 0)
+// internal class Program
 // {
-//     number = number * -1;
+//     private static void Main(string[] args)
+//     {
+//         void SumDigitsInNumber(int number)
+//         {
+//             if (number < 0)
+//             {
+//                 number = number * -1;
+//             }
+//             else if (number < 10)
+//             {
+//                 Console.Write(number + " -> " + number);
+//             }
+//             else if (number >= 10 & number < 100)
+//             {
+//                 int a1 = number % 10;
+//                 int a2 = number / 10;
+//                 int sum = a1 + a2;
+//                 Console.Write(number + " -> " + sum);
+//             }
+//             else if (number >= 100 & number < 1000)
+//             {
+//                 int a1 = number % 10;
+//                 int a2 = number / 10 % 10;
+//                 int a3 = number / 100 % 10;
+//                 int sum = a1 + a2 + a3;
+//                 Console.Write(number + " -> " + sum);
+//             }
+//             else if (number >= 1000 & number < 10000)
+//             {
+//                 int a1 = number % 10;
+//                 int a2 = number / 10 % 10;
+//                 int a3 = number / 100 % 10;
+//                 int a4 = number / 1000 % 10;
+//                 int sum = a1 + a2 + a3 + a4;
+//                 Console.Write(number + " -> " + sum);
+//             }
+//             else
+//             {
+//                 Console.Write(number + "-> " + "Введено много символов. Введите число < 10000 ");
+//             }
+//         }
+//         Console.Clear();
+//         Console.Write("Введите число:");
+//         int num = Convert.ToInt32(Console.ReadLine());
+//         SumDigitsInNumber(num);
+//     }
 // }
-// if (number < 10)
-// {
-//     Console.Write( number + " -> "+ number);
-// }
-// else if (number >= 10 & number < 100)
-// {
-//     int a1 = number % 10;
-//     int a2 = number / 10;
-//     int sum = a1 + a2;
-//     Console.Write( number + " -> "+ sum);
-// }
-// else if (number >= 100 & number < 1000)
-// {
-//     int a1 = number % 10;
-//     int a2 = number / 10 % 10;
-//     int a3 = number /100 % 10;
-//     int sum = a1 + a2 + a3;
-//     Console.Write( number + " -> "+ sum);
-// }
-// else if (number >= 1000 & number < 10000)
-// {
-//     int a1 = number % 10;
-//     int a2 = number / 10 % 10;
-//     int a3 = number /100 % 10;
-//     int a4 = number /1000 % 10;
-//     int sum = a1 + a2 + a3 + a4;
-//     Console.Write( number + " -> "+ sum);
-// }
-// else
-// {
-//     Console.Write( number + "-> "+ "Введено много символов. Введите число < 10000 ");
-// }
-// }
-// Console.Write("Введите число:");
-// int num = Convert.ToInt32(Console.ReadLine());
-// SumDigitsInNumber(num);
 
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         int[] RandomArray()
+//         {
+//             int[] Array = new int[8];
+//             for (int i = 0; i < 8; i++)
+//             {
+//                 Array[i] = new Random().Next(1, 1000 + 1);
+//             }
+//             return Array;
+//         }
+
+//         void PrintArray(int[] array)
+//         {
+//             for (int i = 0; i < array.Length; i++)
+//             {
+//                 Console.Write(array[i] + ", ");
+//             }
+//             Console.WriteLine();
+//         }
+//         Console.Clear();
+//         Console.WriteLine("Массив из 8 случайных элементов:");
+//         int[] NewArray = RandomArray();
+//         PrintArray(NewArray); 
+//     }
+// }
